403cf92bb0d3605fff4850ed7a9f7679a82304ec
Baimukhan Kalamsha <kalamshabs@gmail.com>
2026-05-27 15:02:53 +0300

    add http server stub

--- index.js
+++ index.js
@@ -48,3 +48,15 @@ bot.onText(/\/meme/, (msg) => {
   const random = memes[Math.floor(Math.random() * memes.length)];
   bot.sendMessage(msg.chat.id, random);
 });
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
